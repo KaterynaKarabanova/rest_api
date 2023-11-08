c655cc3ae6f84db48a6905929c208844dbfafab2
Kateryna <ikatya7575@gmaiil.com>
2023-11-08 22:16:09 +0300

    deleted comments

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,22 +1,6 @@
-// const Joi = require("joi");
 const contacts = require("../models/contacts");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
-// const addSchema = Joi.object({
-//   name: Joi.string().min(3).max(30).required(),
-//   email: Joi.string()
-//     .email({
-//       minDomainSegments: 2,
-//     })
-//     .required(),
-//   phone: Joi.string()
-//     .length(13)
-//     .pattern(/^\(\d{3}\)\d{3}-\d{4}$/)
-//     .messages({
-//       "string.pattern.base": `Phone number must use this format (XXX)XXX-XXXX.`,
-//     })
-//     .required(),
-// });
 const getAll = async (req, res) => {
   const allContacts = await contacts.listContacts();
   res.json(allContacts);
